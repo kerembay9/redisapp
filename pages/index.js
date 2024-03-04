5b8e8947be472ddd48a1a1424d4dead7b3b1460b
kerembay9 <kerembay9@gmail.com>
2024-03-04 17:48:44 +0300

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center align-center gap-10">
+    <div className="w-screen h-screen flex flex-col justify-center align-center gap-10 bg-slate-700 text-black">
       <h1>Create a Car</h1>
         <div className="w-screen h-screen flex flex-col justify-center align-center gap-10">
         <CarForm/>
